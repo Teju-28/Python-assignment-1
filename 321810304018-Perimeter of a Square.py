f877f74a3b1fa66526d039a1ca01298e7ead3eb5
Teju-28 <62064600+Teju-28@users.noreply.github.com>
2020-06-18 19:47:37 +0530

    Add files via upload

---
+++ 321810304018-Perimeter of a Square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Perimeter of a Square
+
+# In[2]:
+
+
+A = int(input("Enter length of Square:")) #side of a square
+Perimeter = 4*A                     #perimeter of a square
+print("Perimeter of Square is %d" %Perimeter)
+
+
+# In[ ]:
+
+
+
+
