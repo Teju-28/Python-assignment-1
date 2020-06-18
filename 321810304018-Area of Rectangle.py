0f6adc67d7051f1b2a58d600101fe14d8cb21e73
Teju-28 <62064600+Teju-28@users.noreply.github.com>
2020-06-18 19:36:41 +0530

    Add files via upload

---
+++ 321810304018-Area of Rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Area of Rectangle
+
+# In[1]:
+
+
+L = float(input("Enter the length:"))  #length of rectangle
+B = float(input("Enter the breadth:")) #breadth of rectangle
+Area = L*B
+print("Area of Rectangle is %0.2f:" % Area)
+
+
+# In[ ]:
+
+
+
+
