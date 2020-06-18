a3c7fb04d6c30c1f74480dfb7b0e18ebdaa29866
Teju-28 <62064600+Teju-28@users.noreply.github.com>
2020-06-18 18:37:51 +0530

    Add files via upload

---
+++ 321810304018-Simple Interest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Simple interest
+
+# In[1]:
+
+
+P = 2000    #principal amount
+R = 1       #rate
+T = 2       #time
+
+SI = (P*R*T)/100   #simple interest
+print("Simple interest is ",SI)
+
