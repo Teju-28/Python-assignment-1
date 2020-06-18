e29617cf4b293668f8bcecc42bff08fa548c56a6
Teju-28 <62064600+Teju-28@users.noreply.github.com>
2020-06-18 18:03:20 +0530

    Add files via upload

---
+++ 321810304018-Simple calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Simple calculator
+
+# In[2]:
+
+
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+print("Sum = ",num1+num2)
+print("Difference = ",num1-num2)
+print("Product = ",num1*num2)
+print("Division = ",num1/num2)
+print("Modulus = ",num1%num2)
+print("Exponent = ",num1**num2)
+print("Floor division = ",num1//num2)
+
+
+# In[ ]:
+
+
+
+
