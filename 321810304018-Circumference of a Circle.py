ccc176ff74a5de62b6cf56b37f38b2bce3d66e35
Teju-28 <62064600+Teju-28@users.noreply.github.com>
2020-06-18 19:56:38 +0530

    Add files via upload

---
+++ 321810304018-Circumference of a Circle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Circumference of a Circle
+
+# In[1]:
+
+
+PI = 3.14
+R = float(input("Enter radius of circle:"))  #radius of circle
+Circumference = 2*PI*R
+print("Circumference of a Circle is %.2f" %Circumference)
+
+
+# In[ ]:
+
+
+
+
