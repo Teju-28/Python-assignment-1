7ce23ea969766baf14cdceb21bae5196fffb52fb
Teju-28 <62064600+Teju-28@users.noreply.github.com>
2020-06-18 19:24:08 +0530

    Add files via upload

---
+++ 321810304018-Tempature in Celsius to Fahrenheit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Temperature in Celsius to Fahrenheit
+# 
+
+# In[1]:
+
+
+Celsius = float(input("Enter the tempature"))
+Fahrenheit = (Celsius*9/5)+32
+print("%.2f of Celsius is : %.2f Fahrenheit" %(Celsius,Fahrenheit))
+
