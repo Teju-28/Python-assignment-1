b7f5a0095c3cdfdaf7a58eeebd740e15fc8b37bc
Teju-28 <62064600+Teju-28@users.noreply.github.com>
2020-06-18 20:29:00 +0530

    Add files via upload

---
+++ 321810304018-Swaping of two numbers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Swap two numbers
+
+# In[1]:
+
+
+A = input("Enter the first number:")   #input the numbers
+B = input("Enter the second number:")
+print("Value of A before swapping:",A)  #before swaping
+print("Value of B before swapping:",B)
+temp=A             #temporary variable for swaping
+A=B
+B=temp
+print("Value of A after swapping:",A)  #after swaping
+print("Value of B after swapping:",B)
+
+
+# In[ ]:
+
+
+
+
